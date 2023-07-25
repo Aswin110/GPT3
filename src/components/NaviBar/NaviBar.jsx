de1c27c643257524bcf8423942be991b93bf725c
Aswin110 <aswinashok110@gmail.com>
2023-07-25 21:08:56 +0530

    GPT3
    Created the structure

---
+++ src/components/NaviBar/NaviBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NaviBar.css'
+
+const NaviBar = () => {
+  return (
+    <div>
+      NaviBar
+    </div>
+  )
+}
+
+export default NaviBar
---
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './WhatGPT3.css'
+const WhatGPT3 = () => {
+  return (
+    <div>
+      what GPT-3
+    </div>
+  )
+}
+
+export default WhatGPT3
